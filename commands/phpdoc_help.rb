1b13c467ae8e1d81d36d04e63290254c652dabd3
Shalom Gibly <sgibly@aptana.com>
2010-11-24 17:47:59 -0800

    Replace some problematic characters (#1329)

--- commands/phpdoc_help.rb
+++ commands/phpdoc_help.rb
@@ -14,28 +14,28 @@ Markdown.pl <<'EOF'
 
 This bundle contains snippets for use with [PHPDoc](http://phpdoc.org/).
 
- * `doc_c ` Ñ Class
- * `doc_d ` Ñ Define inline constant
- * `doc_f ` Ñ Function
- * `doc_h ` Ñ Document header
- * `doc_i ` Ñ Interface class
- * `doc_s ` Ñ Abstract function signature
- * `doc_v ` Ñ Class variable
+ * `doc_c ` - Class
+ * `doc_d ` - Define inline constant
+ * `doc_f ` - Function
+ * `doc_h ` - Document header
+ * `doc_i ` - Interface class
+ * `doc_s ` - Abstract function signature
+ * `doc_v ` - Class variable
 
-There is also a command used for adding documentation to existing entities Ð move the caret before a class/interface, function, define or variable and use `doc?`.
+There is also a command used for adding documentation to existing entities, move the caret before a class/interface, function, define or variable and use `doc?`.
 
 # Shell Variables
 
 The snippets make use of two environment variables (_set these under Preferences &#x2192; Advanced &#x2192; Shell Variables_).
 
- * `TM_ORGANIZATION_NAME` Ñ default value of @copyright [+ date]
- * `PHPDOC_AUTHOR` Ñ default value of @author (realname will be used if not set)
+ * `TM_ORGANIZATION_NAME` - default value of @copyright [+ date]
+ * `PHPDOC_AUTHOR` - default value of @author (realname will be used if not set)
 
 If you want docblocks to automatically wrap after 80 columns, you need to set it up:
 
 1. Open the Bundle Editor (&#x2303;&#x2325;&#x2318;B)
-2. Find and select the ÒWrap Docblock at 80 CharactersÓ command
-3. Click in the white box next to ÒKey EquivalentÓ and press space
+2. Find and select the 'Wrap Docblock at 80 Characters' command
+3. Click in the white box next to 'Key Equivalent' and press space
 4. Close the Bundle Editor
 EOF
 end
--- commands/validate_syntax.rb
+++ commands/validate_syntax.rb
@@ -9,7 +9,7 @@ command 'Validate Syntax' do |cmd|
 #!/usr/bin/env ruby
 require ENV['TM_SUPPORT_PATH'] + '/lib/textmate'
 version = %x{#{ENV['TM_PHP'] || 'php'} -v}.split[0..2].join(' ')
-puts "Running syntax check with " + version + "É"
+puts "Running syntax check with " + version + "..."
 result = `#{ENV['TM_PHP'] || 'php'} -d display_errors=on -l`
 puts result.gsub('in -', '')
 TextMate.go_to :line => $1 if result =~ /line (\d+)/
