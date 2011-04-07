dbad105e130d095a37ef87fe9b25b74b9a2d4581
sgtcoolguy <cwilliams@aptana.com>
2011-04-07 15:46:11 -0400

    forcibly remove TM_COMMENT_END and TM_COMMENT_END_2 from env

--- bundle.rb
+++ bundle.rb
@@ -84,7 +84,9 @@ end
 # Special ENV vars for PHP scope
 env 'source.php' do |e|
   e['TM_COMMENT_START'] = '// '
+  e.delete('TM_COMMENT_END')
   e['TM_COMMENT_START_2'] = '# '
+  e.delete('TM_COMMENT_END_2')
   e['TM_COMMENT_START_3'] = '/* '
   e['TM_COMMENT_END_3'] = '*/'
 end
