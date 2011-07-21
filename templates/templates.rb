1269dc8cc83788407f55baf304c60f339088160a
Allen Yeung <ayeung@aptana.com>
2011-07-21 14:34:59 -0700

    Revert "Fix APSTUD-2134:Add .location property to file templates"
    This reverts commit c7360617f6e2b6d5fdad8b5d3d8f43150c063e18.

--- templates/templates.rb
+++ templates/templates.rb
@@ -2,5 +2,9 @@ require 'ruble'
 
 template "PHP Template" do |t|
   t.filetype = "*.php"
-  t.location = "templates/template.php"
+  t.invoke do |context|
+    ENV['TM_DATE'] = Time.now.strftime("%Y-%m-%d")
+    raw_contents = IO.read("#{ENV['TM_BUNDLE_SUPPORT']}/../templates/template.php")
+    raw_contents.gsub(/\$\{([^}]*)\}/) {|match| ENV[match[2..-2]] }
+ end
 end
